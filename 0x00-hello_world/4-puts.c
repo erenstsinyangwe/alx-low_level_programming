5c6a6263df89f10252ce39aeeb7cdff853932bd8
Ernest Sinyangwe <ernest.r.sinyangwe@gmail.com>
2022-09-11 11:36:09 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Description - prints "Programming is like 
+*building a multilingual puzzle
+*Return: always 0 if (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
