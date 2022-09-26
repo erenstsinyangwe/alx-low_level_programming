9668f46a7a77e78baee90579e999746dff556f56
Ernest Sinyangwe <ernest.r.sinyangwe@gmail.com>
2022-09-26 23:29:16 +0000

    fix again

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
 * _strchr - locates a character in a string
-*@s: string to check
-*@c: chararter to check 
-*Return: pointer to spot in $ with c or null
+* @s: string to check
+* @c: chararter to check
+* Return: pointer to spot in $ with c or null
 *
 */
 char *_strchr(char *s, char c)
 {
 	int i;
-	
+
 	for (i = 0; *(s + i); i++)
 	{
 		if (*(s + i) == c)
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+/**
+*
+*
+*
+*
+*
+*\
+unsigned int _strspn(char *s, char *accept)
+{
+	
+}
