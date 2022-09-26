f2d37bfa1083fc97119f0dcb9449b7372f848caa
Ernest Sinyangwe <ernest.r.sinyangwe@gmail.com>
2022-09-26 23:02:16 +0000

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _strchr - locates a character in a string
+*@s: string to check
+*@c: chararter to check 
+*Return: pointer to spot in $ with c or null
+*
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+	
+	for (i = 0; (s + i); i++;)
+	{
+		if (*(s +) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+:x
