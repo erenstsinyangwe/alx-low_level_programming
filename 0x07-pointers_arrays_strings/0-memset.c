42a479947a52e64277da21a4617532beb6fe838b
Ernest Sinyangwe <ernest.r.sinyangwe@gmail.com>
2022-09-26 22:33:28 +0000

    char *_memset(char *s, char b, unsigned int n);

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _memset - fills a memory block wiht a constant byte
+* @s: address to memory block
+* @b: char to be used
+* @n: number of bytes to usedd
+*
+* Return: pointer to the memory block
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
