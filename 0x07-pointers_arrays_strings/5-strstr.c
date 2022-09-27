913bd24a73dadc6ed4da8d8c474d4feb5325d4cb
Ernest Sinyangwe <ernest.r.sinyangwe@gmail.com>
2022-09-27 00:29:05 +0000

    fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,14 +13,14 @@ char *_strstr(char *haystack, char *needle)
 
 	while (haystack[i])
 	{
-		while (needle[j] && (haystack == needle[0]))
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
 			if (haystack[i + j] == needle[j])
 				j++;
 			else
 				break;
 		}
-		if (needle[i])
+		if (needle[j])
 		{
 			i++;
 			j = 0;
