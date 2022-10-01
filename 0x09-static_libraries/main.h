d374b11d1bc14d41011a1c2f7727fbc8ef114e19
Ernest Sinyangwe <ernest.r.sinyangwe@gmail.com>
2022-10-01 22:05:10 +0000

    0x09. C - Static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
- *  * File: main.h
- *   * Auth: Alagwu David
- *    * Desc: Header file containing the prototypes of all functions
- *     *       included in the static library libholberton.a.
- *      */
+
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -26,4 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endif /* MAIN_H */
